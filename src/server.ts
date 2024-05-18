4788e205f282e29d93d10c00942eeade052b8e55
aburahan8721 <aburaihan8721@gmail.com>
2024-05-18 22:09:10 +0600

    basic setup done

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+
+async function main() {
+  try {
+    const { ConnectionStates } = await mongoose.connect(
+      config.database_url_local as string
+    );
+
+    if (ConnectionStates.connected) {
+      console.log('Db is connected');
+    }
+
+    app.listen(config.port, () => {
+      console.log(`App is listening at http://localhost:${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
